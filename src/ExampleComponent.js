62e96825ca590378b5bb205b090d4c557128d83a
github username <github email address>
2019-03-22 21:36:22 -0400

    Done.

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -5,7 +5,7 @@ class ExampleComponent extends Component {
   render() {
     return (
       <div className="example">
-        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="whoa"/>
+        <img src="https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif" alt="whoa"/>
         <p>Whoa!</p>
       </div>
     );
